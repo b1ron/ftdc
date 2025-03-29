d58d110debbfd6651ec7e07318df0c5c6c0db6c5
b1ron <goldingbyron@gmail.com>
2025-03-29 01:29:37 +0100

    browserfication WIP

---
+++ bson_constants.js
@@ -0,0 +1,64 @@
+// BSON Types - https://bsonspec.org/spec.html
+
+// Number BSON type
+export const DATA_NUMBER = 1;
+
+// String BSON type
+export const DATA_STRING = 2;
+
+// Object BSON type @deprecated
+export const DATA_OBJECT = 3;
+
+// Array BSON type
+export const DATA_ARRAY = 4;
+
+// Binary BSON type
+export const DATA_BINARY = 5;
+
+// Undefined BSON type @deprecated
+export const DATA_UNDEFINED = 6;
+
+// ObjectId BSON type
+export const DATA_OBJECTID = 7;
+
+// Boolean BSON type
+export const DATA_BOOLEAN = 8;
+
+// Date BSON type
+export const DATA_DATE = 9;
+
+// null BSON type
+export const DATA_NULL = 10;
+
+// Regular Expression BSON type
+export const DATA_REGEXP = 11;
+
+// DBPointer BSON type
+export const DATA_DBPOINTER = 12;
+
+// JavaScript code BSON type
+export const DATA_CODE = 13;
+
+// Symbol BSON type
+export const DATA_SYMBOL = 14;
+
+// Code with scope BSON type
+export const DATA_CODE_W_SCOPE = 15;
+
+// 32 bit integer BSON type
+export const DATA_INT32 = 16;
+
+// Timestamp BSON type
+export const DATA_TIMESTAMP = 17;
+
+// Long BSON type
+export const DATA_LONG = 18;
+
+// Decimal BSON type
+export const DATA_DECIMAL128 = 19;
+
+// MinKey BSON type
+export const DATA_MIN_KEY = 0xff;
+
+// MaxKey BSON type
+export const DATA_MAX_KEY = 0x7f;
