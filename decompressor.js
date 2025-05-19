82e077526070e77de16a7bf63f3626b3122828f6
b1ron <goldingbyron@gmail.com>
2025-05-19 20:57:52 +0200

    WIP metrics

--- decompressor.js
+++ decompressor.js
@@ -17,7 +17,7 @@ const inflate = async function(buffer, format) {
 
 const uncompress = async function() {
   let data = await fetchFile('https://github.com/b1ron/ftdc/raw/refs/heads/master/files/metrics.bson');
-  const options = {FTDC: true};
+  const options = {FTDC: true}; // FTDC true returns compressed metrics
   data = new Uint8Array(data);
   const compressed = parser.parseBSON(
       data,
@@ -26,9 +26,13 @@ const uncompress = async function() {
 
   options.FTDC = false;
   const metrics = new Uint8Array(await inflate(compressed, 'deflate'));
-  const size = utils.readInt32LE(metrics);
-  const ref = parser.parseBSON(metrics.subarray(0, size), options);
-  console.log(ref);
+  const refSize = utils.readInt32LE(metrics);
+  console.log(parser.parseBSON(metrics.subarray(4, refSize)));
+  console.log(refSize, metrics.length, metrics.length - refSize >= 8); // can we read 64b
+
+  const sampleCount = utils.readUInt32LE(metrics.subarray(refSize, metrics.length));
+  const metricCount = utils.readUInt32LE(metrics.subarray(refSize + 4, metrics.length));
+  console.log(sampleCount, metricCount);
 };
 
 async function fetchFile(uri) {
